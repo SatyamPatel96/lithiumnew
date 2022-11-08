b59fd46ce5ff936e32a063209518559917ca9e43
SatyamPatel96 <patelsatyam9827@gmail.com>
2022-11-08 21:02:18 +0530

    errorhandling

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,29 @@
+const userModel = require("../models/userModel")
+const jwt = require("jsonwebtoken");
+
 const authenticate = function(req, req, next) {
     //check the token in request header
     //validate this token
-
-    next()
+    const token = req.header["x-auth-token"]
+    if (!token) return res.status(400).send({ status: false, msg: "token must be present" });
+    else {
+        let decodedToken = jwt.verify(token, "functionup-thorium");
+        if (!decodedToken)
+            return res.status(400).send({ status: false, msg: "token is invalid" })
+        else {
+            req.decodedToken = decodedToken
+            next()
+        }
+    }
 }
 
-
-const authorise = function(req, res, next) {
+const authorise = async function(req, res, next) {
     // comapre the logged in user's id and the id in request
-    next()
+    let userId = req.body.emailId
+    let password = req.body.password
+    const valid = await userModel.findOne({ emailId: userId, password: password })
+    if (valid) next()
+    else res.status(400).send({ msg: "please enter valid userName and Password" })
 }
+
+module.exports = { authenticate, authorise }
